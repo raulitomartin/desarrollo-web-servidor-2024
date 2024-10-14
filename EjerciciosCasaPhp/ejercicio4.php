6467598f0be0d213ae82f9e1a9790ae6344466fc
raulitomartin <raulmartingonzalez2@gmail.com>
2024-10-14 16:49:23 +0200

    ejercicio 4 actualizado

--- EjerciciosCasaPhp/ejercicio4.php
+++ EjerciciosCasaPhp/ejercicio4.php
@@ -5,35 +5,78 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pasar de celsius a farenheit y viceversa</title>
     <?php
-        error_reporting( E_ALL );
-        ini_set("display_errors", 1 );  
+/*         error_reporting( E_ALL );
+        ini_set("display_errors", 1 );   */
     ?>
 </head>
 <body>
     <form action="" method="post">
+
         <label for="temperatura">Temperatura</label>
         <input type="text" name="temperatura" id="temperatura">
-        <br>
-        <br>
         <select name="tiposTemperatura">
             <option value="celsius">Celsius</option>
             <option value="farenheit">Farenheit</option>
+            <option value="kelvin">Kelvin</option>
         </select>
+
+
         <br>
         <br>
+
+
+        <label for="convertir">Convertir a:</label>
+        <select name="conversionTemperatura" id="convertir">
+            <option value="conversionCelsius">Celsius</option>
+            <option value="conversionFarenheit">Farenheit</option>
+            <option value="conversionKelvin">Kelvin</option>
+
+            </select>
+        <br>
+        <br>
+
+
         <input type="submit" value="CALCULAR">
     </form>
     <?php
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $temperatura = $_POST['temperatura'];
-            $tiposTemperatura = $_POST['tiposTemperatura'];
+            $temperatura = $_POST["temperatura"];
+            $tiposTemperatura = $_POST["tiposTemperatura"];
+            $conversionTemperatura = $_POST["conversionTemperatura"];
+            
+            switch ($tiposTemperatura) {
+                case 'celsius':
+                    $tempCelsius = $temperatura;
+                    break;
+                case 'farenheit':
+                    $tempFarenheit = ($temperatura - 32) * 5 / 9;
+                    break;
+                case 'kelvin':
+                    $tempKelvin = $temperatura - 273.15;
+                    break;
+                default:
+                    # code...
+                    break;
+            }
+            
+            
+            switch ($conversionTemperatura) {
+                case 'celsius':
+                    $conversion = $tempCelsius;
+                    break;
+                case 'farenheit':
+                    $conversion = ($tempCelsius * 9 / 5) + 32;
+                    break;
+                case 'kelvin':
+                    $conversion = $tempCelsius = 273.15;
+                    break;
+                default:
+                    # code...
+                    break;
+            }
 
-            $transformacion = match ($tiposTemperatura) {
-                 'celsius'=> (($temperatura - 32) *5 / 9),
-                 'farenheit'=>(($temperatura * 9 / 5) + 32),
-            };                        
+            echo "<h3>La temperatura convertida es: $conversion</h3>";
         }
-        echo "<h2>La temperatura es:$transformacion</h2>";
     ?>
 </body>
 </html>
