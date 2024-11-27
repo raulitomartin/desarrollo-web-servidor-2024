49494d2f0308b83ddcf9989341fca4655ad3b0e6
raulitomartin <raulmartingonzalez2@gmail.com>
2024-11-27 09:31:51 +0100

    Raulito ahora toca ponerse las pilas

--- BaseDeDatos/animes/index.php
+++ BaseDeDatos/animes/index.php
@@ -4,74 +4,86 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index de Animes</title>
-    <?php
-        error_reporting(E_ALL);
-        ini_set("display_errors", 1);
-
-    require('conexion.php');
-    ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );    
+
+        require('conexion.php');
+
+        session_start();
+        if(isset($_SESSION["usuario"])) {
+            echo "<h2>Bienvenid@ " . $_SESSION["usuario"] . "</h2>";
+        }else{
+            header("location: usuario/iniciar_sesion.php");
+            exit;
+        }
+    ?>
 </head>
 <body>
-    <div class = "container">
-    <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $id_anime = $_POST["id_anime"];
-            echo "<h1>$id_anime</h1>";
-            $sql = "DELETE FROM animes WHERE id_anime = $id_anime";
-            $_conexion -> query($sql);
-        }
-    ?>  
+    <div class="container">
+        <a class="btn btn-warning" href="usuario/cerrar_sesion.php">Cerrar sesión</a>
+        <h1>Tabla de animes</h1>
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST") {
+                $id_anime = $_POST["id_anime"];
+                echo "<h1>$id_anime</h1>";
+                //  borrar el anime
+                $sql = "DELETE FROM animes WHERE id_anime = $id_anime";
+                $_conexion -> query($sql);
+            }
 
-
-
-    
-    <h1>Tabla de animes</h1>
-    <?php
-        $sql = "SELECT * FROM animes";
-        $resultado = $_conexion -> query($sql); //El reultado almacena una consulta
+            $sql = "SELECT * FROM animes";
+            $resultado = $_conexion -> query($sql);
+            /**
+             * Aplicamos la función query a la conexión, donde se ejecuta la sentencia SQL hecha
+             * 
+             * El resultado se almacena $resultado, que es un objeto con una estructura parecida
+             * a los arrays
+             */
         ?>
-        <a href="nuevo_anime.php" class="btn btn-primary">Crear nuevo anime</a><br><br>
-        <table class = "table table-striped table-hover">
-            <thead class ="table-dark">
-                <th>Titulo</th>
-                <th>Estudio</th>
-                <th>Año</th>
-                <th>Numero de temporadas</th>
-                <th>Imagen</th>
-                <th>Borrar</th>
-            </thead >
+        <a class="btn btn-secondary" href="nuevo_anime.php">Crear nuevo anime</a><br><br>
+        <table class="table table-striped table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>Título</th>
+                    <th>Estudio</th>
+                    <th>Año</th>
+                    <th>Número de temporadas</th>
+                    <th>Imagen</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
             <tbody>
                 <?php
-                    while ($fila = $resultado -> fetch_assoc()) { //trata el resultado como un array asociativo
+                    while($fila = $resultado -> fetch_assoc()) {    // trata el resultado como un array asociativo
                         echo "<tr>";
-                        echo "<td>" . $fila['titulo'] . "</td>";
-                        echo "<td>" . $fila['nombre_estudio'] . "</td>";
-                        echo "<td>" . $fila['anno_estreno'] . "</td>";
-                        echo "<td>" . $fila['num_temporadas'] . "</td>";
+                        echo "<td>" . $fila["titulo"] . "</td>";
+                        echo "<td>" . $fila["nombre_estudio"] . "</td>";
+                        echo "<td>" . $fila["anno_estreno"] . "</td>";
+                        echo "<td>" . $fila["num_temporadas"] . "</td>"; 
                         ?>
                         <td>
-                            <img width = "100" height = "200" src = "<?php echo $fila["imagen"]?>">
+                            <img width="100" height="200" src="<?php echo $fila["imagen"] ?>">
                         </td>
                         <td>
-                            <a class = "btn btn-primary" href="ver_anime.php?id_anime= <?php echo $fila["id_anime"]?>">Editar</a>
+                            <a class="btn btn-primary" 
+                               href="ver_anime.php?id_anime=<?php echo $fila["id_anime"] ?>">Editar</a>
                         </td>
                         <td>
-                            <form action="" method = "post">
-                                <input class = "btn btn-danger" type = "submit" value = "Borrar">
-                                <input type = "hidden" name = "id_anime" value = "<?php echo $fila["id_anime"]?>">
+                            <form action="" method="post">
+                                <input type="hidden" name="id_anime" value="<?php echo $fila["id_anime"] ?>">
+                                <input class="btn btn-danger" type="submit" value="Borrar">
                             </form>
-                        </td> 
+                        </td>
                         <?php
                         echo "</tr>";
-                                                
                     }
                 ?>
-                
-                
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
             </tbody>
         </table>
-        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- BaseDeDatos/animes/ver_anime.php
+++ BaseDeDatos/animes/ver_anime.php
@@ -22,28 +22,23 @@
 </head>
 <body>
     <div class="container">
-        <h1>Nuevo anime</h1>
+        <h1>Editar anime</h1>
         <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST") {
-            $titulo = $_POST["titulo"];
-            $nombre_estudio = $_POST["nombre_estudio"];
-            $anno_estreno = $_POST["anno_estreno"];
-            $num_temporadas = $_POST["num_temporadas"];
-            /**
-             * $_FILES -> que es un array BIDIMENSIONAL
-             */
-            //var_dump($_FILES["imagen"]);
-            $nombre_imagen = $_FILES["imagen"]["name"];
-            $ubicacion_temporal = $_FILES["imagen"]["tmp_name"];
-            $ubicacion_final = "./imagenes/$nombre_imagen";
+        //echo "<h1>" . $_GET["id_anime"] . "</h1>";
 
-            move_uploaded_file($ubicacion_temporal, $ubicacion_final);
-
-            $sql = "INSERT INTO animes (titulo, nombre_estudio, anno_estreno, num_temporadas, imagen) 
-                VALUES ('$titulo', '$nombre_estudio', $anno_estreno, $num_temporadas, '$ubicacion_final')";
-
-            $_conexion -> query($sql);
+        $id_anime = $_GET["id_anime"];
+        $sql = "SELECT * FROM animes WHERE id_anime = $id_anime";
+        $resultado = $_conexion -> query($sql);
+        
+        while($fila = $resultado -> fetch_assoc()) {
+            $titulo = $fila["titulo"];
+            $nombre_estudio = $fila["nombre_estudio"];
+            $anno_estreno = $fila["anno_estreno"];
+            $num_temporadas = $fila["num_temporadas"];
+            $imagen = $fila["imagen"];
         }
+
+        //echo "<h1>$titulo</h1>";
 
         $sql = "SELECT * FROM estudios ORDER BY nombre_estudio";
         $resultado = $_conexion -> query($sql);
@@ -52,18 +47,33 @@
         while($fila = $resultado -> fetch_assoc()) {
             array_push($estudios, $fila["nombre_estudio"]);
         }
-        //print_r($estudios);
- 
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $id_anime = $_POST["id_anime"];
+            $titulo = $_POST["titulo"];
+            $nombre_estudio = $_POST["nombre_estudio"];
+            $anno_estreno = $_POST["anno_estreno"];
+            $num_temporadas = $_POST["num_temporadas"];
+
+            $sql = "UPDATE animes SET
+                titulo = '$titulo',
+                nombre_estudio = '$nombre_estudio',
+                anno_estreno = $anno_estreno,
+                num_temporadas = $num_temporadas
+                WHERE id_anime = $id_anime
+            ";
+            $_conexion -> query($sql);
+        }
         ?>
         <form class="col-6" action="" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label class="form-label">Título</label>
-                <input class="form-control" type="text" name="titulo">
+                <input class="form-control" type="text" name="titulo" value="<?php echo $titulo ?>">
             </div>
             <div class="mb-3">
                 <label class="form-label">Nombre estudio</label>
                 <select class="form-select" name="nombre_estudio">
-                    <option value="" selected disabled hidden>--- Elige el estudio ---</option>
+                    <option value="<?php echo $nombre_estudio ?>" selected hidden><?php echo $nombre_estudio ?></option>
                     <?php
                     foreach($estudios as $estudio) { ?>
                         <option value="<?php echo $estudio ?>">
@@ -74,18 +84,19 @@
             </div>
             <div class="mb-3">
                 <label class="form-label">Año estreno</label>
-                <input class="form-control" type="text" name="anno_estreno">
+                <input class="form-control" type="text" name="anno_estreno" value="<?php echo $anno_estreno ?>">
             </div>
             <div class="mb-3">
                 <label class="form-label">Número de temporadas</label>
-                <input class="form-control" type="text" name="num_temporadas">
+                <input class="form-control" type="text" name="num_temporadas" value="<?php echo $num_temporadas ?>">
             </div>
             <div class="mb-3">
                 <label class="form-label">Imagen</label>
                 <input class="form-control" type="file" name="imagen">
             </div>
             <div class="mb-3">
-                <input class="btn btn-primary" type="submit" value="Insertar">
+                <input type="hidden" name="id_anime" value="<?php echo $id_anime ?>">
+                <input class="btn btn-primary" type="submit" value="Confirmar">
                 <a class="btn btn-secondary" href="index.php">Volver</a>
             </div>
         </form>
